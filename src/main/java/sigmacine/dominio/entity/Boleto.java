66301a8aa846a467808ee2655c8db6c8b83300f2
CarolinaUjueta <c_ujueta@javeriana.edu.co>
2025-10-05 23:41:40 -0500

    Corrección clases

--- src/main/java/sigmacine/dominio/entity/Boleto.java
+++ src/main/java/sigmacine/dominio/entity/Boleto.java
@@ -1,5 +1,46 @@
 package sigmacine.dominio.entity;
 
 public class Boleto {
+    private int id;
+    private Silla silla;
+    private double precio;
+    private String estado;
+
+    public Boleto() {}
+
+    public Boleto(int id, double precio, String estado) {
+        this.id = id;
+        this.silla = new Silla();
+        this.precio = precio;
+        this.estado = estado;
+    }
+
+    public int getId() { 
+        return id; 
+    }
+    public void setId(int id) { 
+        this.id = id; 
+    }
+
+    public Silla getSilla() { 
+        return silla; 
+    }
+    public void setAsiento(Silla silla) { 
+        this.silla = silla; 
+    }
+
+    public double getPrecio() { 
+        return precio; 
+    }
+    public void setPrecio(double precio) { 
+        this.precio = precio; 
+    }
+
+    public String getEstado() { 
+        return estado; 
+    }
+    public void setEstado(String estado) { 
+        this.estado = estado; 
+    }
 
 }

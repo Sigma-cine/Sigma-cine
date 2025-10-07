6a289296896880487070d53bfae0e02a957fc360
CarolinaUjueta <c_ujueta@javeriana.edu.co>
2025-10-06 21:23:20 -0500

    Mover clases a rama entities

--- src/main/java/sigmacine/dominio/entity/Boleto.java
+++ src/main/java/sigmacine/dominio/entity/Boleto.java
@@ -1,27 +1,5 @@
 package sigmacine.dominio.entity;
 
 public class Boleto {
-    private Long id;
-    private String pelicula;
-    private String sala;
-    private String horario;
-    private String asiento;
-    private long precio;
-
-
-    public String getPelicula() {
-        return pelicula;
-    }
-
-    public String getSala() {
-        return sala;
-    }
-
-    public String getHorario() {
-        return horario;
-    }
     
-    public long getPrecio() {
-        return precio;
-    }
 }
